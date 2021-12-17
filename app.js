98b7b9510fe59a6f332bf23d0253828438a4cb40
Dee Adebayo <deeadebayo@gmail.com>
2021-12-17 02:06:08 -0500

    js mvp

---
+++ app.js
@@ -0,0 +1,68 @@
+const getResultsBtn = document.getElementById('getResults')
+const resultsTable = document.getElementById('results-table')
+
+let retrievedUsers = []
+const getRandomUsers = fetch('https://randomuser.me/api/?results=10')
+	.then(res => res.json())
+	.then(({ results }) => retrievedUsers.push(...results))
+	.catch(err => console.warn(`Here's the error: ${err}`))
+
+const htmlTagMaker = (
+	element,
+	attribute,
+	attributeValue,
+	text = false,
+	contentText = ''
+) => {
+	let result = document.createElement(element)
+	result.setAttribute(attribute, attributeValue)
+	text ? (result.textContent = contentText) : result
+	return result
+}
+
+getResultsBtn.addEventListener('click', () => {
+	let sessionUsers = retrievedUsers.map(
+		({ name: { first, last }, email, picture: { thumbnail } }) => {
+			return { first, last, email, thumbnail }
+		}
+	)
+
+	if (sessionUsers.length > 0) {
+		let { first, last, email, thumbnail } =
+			sessionUsers[sessionUsers.length - 1]
+
+		let resultsWrapper = htmlTagMaker('div', 'class', 'results__result-item')
+
+		let imageWrapperDetails = htmlTagMaker(
+			'div',
+			'class',
+			'results__results-item__details__image',
+			false
+		)
+
+		let personNameDetails = htmlTagMaker(
+			'div',
+			'class',
+			'results__results-item__details__name',
+			true,
+			`${first} ${last}`
+		)
+
+		let emailDetails = htmlTagMaker(
+			'div',
+			'class',
+			'results__results-item__details__email',
+			true,
+			email
+		)
+
+		imageWrapperDetails.append(htmlTagMaker('img', 'src', thumbnail))
+		resultsWrapper.append(imageWrapperDetails, personNameDetails, emailDetails)
+		resultsTable.append(resultsWrapper)
+		retrievedUsers.pop()
+	} else {
+		alert(
+			'Random User Generator Jam is actively enhancing our webapp to support more randomly generated users. Come work for us!'
+		)
+	}
+})
---
+++ instructions.md
@@ -0,0 +1,20 @@
+Requirements
+Build a simple web application
+Integrate with a REST Web API (https://randomuser.me/api/?results=10)
+Create a user interface around the API, which includes:
+a button to get the next set of results
+display the results as a box per user information, each box should include at least:
+first name
+last name
+picture
+email
+
+        style each box the whole list to your liking
+
+    you can use any resource (including help from others) - but do keep in mind you will have to explain what you have done
+
+    you can use any browser-based JavaScript-based technology
+
+Submission
+To submit the result, please zip up the source code and email back as a response.
+Alternatively, provide a source-control link (e.g. GitHub)
